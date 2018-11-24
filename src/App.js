ee6c20eff81e531e43597c067e3bdea1e3b99125
oyejohnson <johnsonoye34@gmail.com>
2018-11-24 22:25:34 +0100

    reformatedd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from "react";
 import Axios from "axios";
 import url from "./urls";
-
+import removeWhiteSpaces from "./utils";
 // Forms generator
 // Not my best, but am open to better solutions
 
 class Forms extends Component {
   constructor(props) {
     super(props);
+    this.dict = {};
     this.state = {
       insuranceList: [],
       chosen: false,
       riskList: [],
       riskTitle: "",
-      isClicked: false
+      isClicked: false,
+      riskData: {}
     };
     this.onSubmit = this.onSubmit.bind(this);
     this.handleDismiss = this.handleDismiss.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
-
   // We first get the list of risk from our api
   componentDidMount() {
     Axios.get(`${url}/api/r_risk/`).then(res =>
@@ -34,19 +36,32 @@ class Forms extends Component {
   }
 
   onClicked(id) {
+    this.dict = {}
     Axios.get(`${url}/api/r_risk/${id}`).then(res =>
       this.setState({
         riskList: res.data.datatypes,
         riskTitle: res.data.title,
-        chosen: true
+        riskData: {},
+        chosen: true,
+        isClicked:false
       })
     );
+  }
+
+  onChange(event) {
+    event.preventDefault();
+    this.dict[event.target.name] = event.target.value;
+    this.setState({
+      riskData: this.dict
+    });
   }
   onSubmit(event) {
     event.preventDefault();
     this.setState({
-      isClicked: true
+      isClicked: true,
+      riskData: this.dict
     });
+    // console.log(this.state.riskData)
   }
 
   render() {
@@ -55,8 +70,10 @@ class Forms extends Component {
       riskList,
       riskTitle,
       isClicked,
-      insuranceList
+      insuranceList,
+      riskData
     } = this.state;
+    console.log(riskData);
     return (
       <div className="col-md-6 offset-md-3 mt-4">
         <h4 className="text-center mb-3">{riskTitle}</h4>
@@ -100,7 +117,11 @@ class Forms extends Component {
                           : "text"
                       }`,
                       placeholder: data.substring(0, data.indexOf(":")),
-                      className: "form-control"
+                      className: "form-control",
+                      name: removeWhiteSpaces(
+                        data.substring(0, data.indexOf(":")).toLowerCase()
+                      ),
+                      onChange: this.onChange
                     })
                   )
                 ),
@@ -116,11 +137,33 @@ class Forms extends Component {
           <h2 className="text-center mt-2">Data model</h2>
           {insuranceList.map((data, key) => (
             <li key={key}>
-              <a className="s" onClick={() => this.onClicked(data.id)} href='/#'>
+              <a
+                className="s"
+                onClick={() => this.onClicked(data.id)}
+                href="/#"
+              >
                 {data.title} {data.id} f
               </a>
             </li>
           ))}
+          <br />
+
+          {isClicked ? (
+            <div className="parent">
+              <h2 className="text-center mt-2">Form submitted</h2>
+              <div className="parsed-data">
+                {Object.entries(riskData).map(([key, value]) => {
+                  return (
+                    <li key={key}>
+                      <b>{key}:</b> {value}
+                    </li>
+                  );
+                })}
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     );
@@ -128,4 +171,3 @@ class Forms extends Component {
 }
 
 export default Forms;
-
---
+++ src/utils.js
@@ -0,0 +1,6 @@
+
+let removeWhiteSpaces = (string) => {
+    return string.replace(/\s/g, "_");
+}
+
+export default removeWhiteSpaces;
